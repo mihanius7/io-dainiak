1a17294d725ac92156ade74b025087a928bf65be
Mihas Dainiak <mihanius@gmail.com>
2022-04-04 18:29:02 +0300

    First file opening

--- src/com/belhard/io/Task1.java
+++ src/com/belhard/io/Task1.java
@@ -1,7 +1,16 @@
 package com.belhard.io;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.sql.SQLOutput;
+
 public class Task1 {
     public static void main(String[] args) {
-
+        try {
+            FileReader fileReader = new FileReader("resources\\in\\text.txt");
+            System.out.println("File is opened!");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
