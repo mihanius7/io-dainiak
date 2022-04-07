37e7e61dfc7d5079bd536ab1107e65e3fd318366
Mihas Dainiak <mihanius@gmail.com>
2022-04-07 11:23:22 +0300

    Methods renamed. First copying.

--- src/com/belhard/io/util/FileUtil.java
+++ src/com/belhard/io/util/FileUtil.java
@@ -1,15 +1,14 @@
 package com.belhard.io.util;
 
+import java.io.*;
 import java.nio.file.Files;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FileUtil {
-    public static String getContentFromFile(String fileName) {
+    public static String getTextContentFromFile(String fileName) {
         StringBuilder content = new StringBuilder();
         try (FileReader fileReader = new FileReader(fileName)) {
             char[] buffer = new char[256];
@@ -38,5 +37,41 @@ public class FileUtil {
             e.printStackTrace();
         }
 
+    }
+
+    public static int[] getBytesContentFromFile(String fileName) {
+        List<Integer> bytesList = new ArrayList<>();
+        try (FileInputStream input = new FileInputStream(fileName)) {
+            int readByte = 0;
+            while ((readByte = input.read()) != -1) {
+                bytesList.add(readByte);
+            }
+        } catch (IOException openingException) {
+            openingException.printStackTrace();
+        }
+        int[] bytesArray = new int[bytesList.size()];
+        for (int i = 0; i < bytesArray.length; i++) {
+            bytesArray[i] = bytesList.get(i);
+        }
+        System.out.println("file size: " + bytesArray.length);
+        return bytesArray;
+    }
+
+    public static void saveToFile(int[] content, String fileName) {
+        File file = new File(fileName);
+        Path path = Paths.get(fileName).getParent();
+        try {
+            if (!Files.exists(path)) {
+                Files.createDirectories(path);
+            }
+            FileOutputStream output = new FileOutputStream(file);
+            for (int i = 0; i < content.length; i++) {
+                output.write(content[i]);
+            }
+            output.flush();
+            output.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
