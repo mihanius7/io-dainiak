0781f54a9eb427b8586055c14e84c202170d88ac
Mihas Dainiak <mihanius@gmail.com>
2022-04-07 11:22:58 +0300

    First copying tested.

--- src/com/belhard/io/Task2.java
+++ src/com/belhard/io/Task2.java
@@ -1,7 +1,18 @@
 package com.belhard.io;
 
+import com.belhard.io.util.FileUtil;
+
+import java.io.File;
+
 public class Task2 {
     public static void main(String[] args) {
-
+        int[] arr = FileUtil.getBytesContentFromFile("resources\\in\\my-code-dog-standing-on-cans-indian-guys-on-youtube-stack-overflow-luck-online-forums-from-2008.jpg");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + ", ");
+            if (i % 30 == 0 && i != 0) {
+                System.out.println();
+            }
+        }
+        FileUtil.saveToFile(arr, "resources\\out\\my-code-dog-standing-on-cans-indian-guys-on-youtube-stack-overflow-luck-online-forums-from-2008.jpg");
     }
 }
