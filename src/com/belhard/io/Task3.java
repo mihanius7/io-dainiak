5d32f5ea793f61abdd692e3567f3bdf650ff81a7
Mihas Dainiak <mihanius@gmail.com>
2022-04-11 11:44:35 +0300

    All files regexp optionally added

--- src/com/belhard/io/Task3.java
+++ src/com/belhard/io/Task3.java
@@ -3,7 +3,8 @@ package com.belhard.io;
 import com.belhard.io.util.FileUtil;
 
 public class Task3 {
-    public static void main (String args[]) {
-        FileUtil.copyFiles(".*\\.jpeg|.*\\.jpg", "resources\\in\\", "resources\\out2\\");
+    public static void main (String[] args) {
+        FileUtil.copyFiles(".*\\.jpeg|.*\\.jpg", "resources\\in\\", "resources\\out\\");
+//        FileUtil.copyFiles(".*", "resources\\in\\", "resources\\out\\");
     }
 }
